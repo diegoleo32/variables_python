987c3dcb12dadd4fec39be9a968e7d94a7faab11
diegoleo32 <72045749+diegoleo32@users.noreply.github.com>
2022-06-29 11:22:56 -0300

    ejercicios completados de variables modulo 2

---
+++ ejercicios_practica/variablespractica.py
@@ -0,0 +1,29 @@
+# Tipos de variables [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos variables de texto
+
+# Ingrese dos palabras y arme combinaciones con ella
+from operator import sub
+
+
+print('Ingrese palabra 1:')
+palabra_1 = str(input()) #peronismo
+
+print('Ingrese palabra 2:')
+palabra_2 = str(input()) #romina
+
+sub_text= palabra_1[0:3]
+sub_text1= palabra_2[0:2]
+print (sub_text+sub_text1)#perro
+
+# De la primera palabra tome las primeras tres letras, utilice el operador :
+# De la segunda palabra tome las primeras dos letras, utilice el operador :
+# Formar una nueva palabra con los recortes solicitados
+# Imprima en pantalla los resultados
